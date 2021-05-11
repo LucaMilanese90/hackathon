1471f12b26a906efd20588def026406c6784d02f
Luca <luca.milanese90@gmail.com>
2021-05-11 15:51:47 +0200

    added modal component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import './App.css';
 import { useState } from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Modal from 'react-modal';
 import Questions from './components/Questions';
 import Grid from './components/Grid';
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+};
 
 function App() {
   const [position, setPosition] = useState([7, 2]);
   const [mapA, setMapA] = useState([[]]);
+
+  const [modalIsOpen, setIsOpen] = useState(false);
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  // function afterOpenModal() {
+  //   // references are now sync'd and can be accessed.
+  //   subtitle.style.color = '#f00';
+  // }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
 
   return (
     <div className="App">
@@ -16,6 +42,14 @@ function App() {
         setPosition={setPosition}
       />
       <Grid position={position} mapA={mapA} />
+      <button onClick={openModal}>Open Modal</button>
+      <Modal
+        isOpen={modalIsOpen}
+        /* onAfterOpen={afterOpenModal} */
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      ></Modal>
     </div>
   );
 }
