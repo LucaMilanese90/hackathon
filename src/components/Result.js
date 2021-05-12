1875c8d1d967d058e0e2f77a3a87233093ae33d2
sofia <s@sofias.fritz.box>
2021-05-12 11:00:50 +0200

    result page

--- src/components/Result.js
+++ src/components/Result.js
@@ -2,33 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import './Result.css';
 
-const guides = [
-  {
-    id: 1,
-    text: 'One Question per day will guide Percy closer to Ginny',
-    url: './questions',
-    link: 'start-link1',
-  },
-  {
-    id: 2,
-    text: 'For every correct answer Ginny sends one GPS-signal and...',
-    url: './questions',
-    link: 'start-link2',
-  },
-  {
-    id: 3,
-    text: 'she has only power for 7 more signals..',
-    url: './questions',
-    link: 'start-link3',
-  },
-  {
-    id: 4,
-    text: 'Help Percy find Ginny',
-    url: './questions',
-    link: 'start-link4',
-  },
-  { id: 5, text: 'Start', url: './questions', link: 'start-link5' },
-];
 
 const Result = ({ message }) => {
   const history = useHistory();
@@ -48,12 +21,10 @@ const Result = ({ message }) => {
 
   return (
     <div className="start-container-result">
-      <div className="start-article">
-        <h1>{message ? 'Mission Accomplished!' : 'Mission failed!'}</h1>
-        <p
-        /* className={guides[index].link}
-          onClick={() => history.push('/questions')} */
-        >
+       <h1 className="title-result">{message ? 'Mission Accomplished!' : 'Mission failed!'}</h1>
+      <div className="start-article-result">
+       
+        <p>
           {message ? `Here you can follow the whole Mars Mission` : ``}
         </p>
         <p className="start-link5" onClick={() => handleClick}>
