9d6a79087f07e602bcd27876096ccd5bf6c6911a
Luca <luca.milanese90@gmail.com>
2021-08-25 21:04:31 +0200

    test verification commit

--- src/components/Start.js
+++ src/components/Start.js
@@ -1,42 +1,64 @@
-import React, { useEffect, useState } from 'react';
-import { useHistory } from 'react-router-dom';
-import './Start.css';
+import React, { useEffect, useState } from 'react'
+import { useHistory } from 'react-router-dom'
+import './Start.css'
 
 const guides = [
-  {id:1, text: 'One Question per day will guide Percy closer to Ginny', url:'./questions', link:'start-link1'},
-  {id:2, text: 'For every correct answer Ginny sends one GPS-signal and...', url:'./questions', link:'start-link2'},
-  {id:3, text: 'she has only power for 7 more signals..', url:'./questions', link:'start-link3'},
-  {id:4, text: 'Help Percy find Ginny', url:'./questions', link:'start-link4'},
-  {id:5, text: 'Start', url:'./questions', link:'start-link5'}
-];
+  {
+    id: 1,
+    text: 'One Question per day will guide Percy closer to Ginny',
+    url: './questions',
+    link: 'start-link1',
+  },
+  {
+    id: 2,
+    text: 'For every correct answer Ginny sends one GPS-signal and...',
+    url: './questions',
+    link: 'start-link2',
+  },
+  {
+    id: 3,
+    text: 'she has only power for 7 more signals..',
+    url: './questions',
+    link: 'start-link3',
+  },
+  {
+    id: 4,
+    text: 'Help Percy find Ginny',
+    url: './questions',
+    link: 'start-link4',
+  },
+  { id: 5, text: 'Start', url: './questions', link: 'start-link5' },
+]
 
 const Start = (props) => {
-  const history = useHistory();
+  const history = useHistory()
 
-  const [index, setIndex] = useState(0);
-  const classLink = 'Classname';
+  const [index, setIndex] = useState(0)
+  const classLink = 'Classname'
 
   useEffect(() => {
-      const interval = setInterval(() => {
-        if(index<=3){
-        setIndex(index + 1);}
-      }, 3000);
-      return()=> {
-        clearInterval(interval);
-     
-      };
-    }) 
+    const interval = setInterval(() => {
+      if (index <= 3) {
+        setIndex(index + 1)
+      }
+    }, 3000)
+    return () => {
+      clearInterval(interval)
+    }
+  })
   return (
-    <div className='start-container'
-     >
-     <div className="start-article">
-      <h1>Pathfinders</h1>
-      <p className={guides[index].link}
-      onClick={() => history.push('/questions')}>
-        {guides[index].text}</p>
+    <div className="start-container">
+      <div className="start-article">
+        <h1>Pathfinders</h1>
+        <p
+          className={guides[index].link}
+          onClick={() => history.push('/questions')}
+        >
+          {guides[index].text}
+        </p>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Start;
+export default Start
